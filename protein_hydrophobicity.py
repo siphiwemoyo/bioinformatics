1e45dbd255ede3c2bc7bccb59f6dc5341be5cbb3
Siphiwe Moyo <quincybinx@gmail.com>
2023-06-03 10:18:55 +0200

    commit

--- protein_hydrophobicity.py
+++ protein_hydrophobicity.py
@@ -5,6 +5,7 @@ from PIL import Image
 
 seq = 'AGCTCGCTCGCTGCGTATAAAATCGCATCGCGCGCAGC'
 protein_seq = 'IRTNGTHMQPLLKLMKFQKFLLELFTLQKRKPEKGYNLPIISLNQ'
+
 GES_Scale = {'F':-3.7,'M':-3.4,'I':-3.1,'L':-2.8,'V':-2.6,
              'C':-2.0,'W':-1.9,'A':-1.6,'T':-1.2,'G':-1.0,
              'S':-0.6,'P': 0.2,'Y': 0.7,'H': 3.0,'Q': 4.1,
